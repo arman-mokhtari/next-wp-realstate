7be7706889b2a59fe17ac94cb63448c98bfa2a6c
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-04-15 17:33:22 +0330

    implement the cta

--- components/mainMenu/MainMenu.js
+++ components/mainMenu/MainMenu.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { FaHouseUser, FaHeart } from "react-icons/fa";
 
-export const MainMenu = ({ items }) => {
+export const MainMenu = ({
+  items,
+  callToActionDestination,
+  callToActionLabel,
+}) => {
   return (
     <div className="bg-slate-800 text-white px-5 h-[64px] sticky top-0 z-20 flex">
       <div className="flex py-4 pl-5 text-pink-600">
@@ -35,6 +39,11 @@ export const MainMenu = ({ items }) => {
               </div>
             </div>
           ))}
+        <div className="ml-3 my-auto">
+          <div className="bg-pink-500 hover:bg-pink-700 inline-block my-2 px-4 py-2 uppercase rounded-md cursor-pointer font-bold text-white">
+            <Link href={callToActionDestination}>{callToActionLabel}</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
