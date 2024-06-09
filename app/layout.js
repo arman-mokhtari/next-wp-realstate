c681cdebdbe0ecbb2452ff41b0019de23c24699c
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-06-09 09:11:02 +0330

    tw css n next font fixed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,22 @@
+import { Poppins, Aboreto } from "next/font/google";
+import "../styles/globals.css";
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+
+const aboreto = Aboreto({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  variable: "--font-aboreto",
+});
 const Layout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${poppins.variable} ${aboreto.variable}`}>
+      <body className="font-body">{children}</body>
     </html>
   );
 };
